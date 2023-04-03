888513ddde0b1400d2e197c38264d113ecbb430b
Vishal Sindham <37668661+VishalSindham@users.noreply.github.com>
2023-04-03 11:13:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const tagsEL = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value =''
+        }, 10)
+        randomSelect()
+    }
+})
+
+const createTags = (input) =>  {
+    const tags = input.split(",").filter(tag => tag.trim()
+    !== '').map(tag => tag.trim())
+    
+    tagsEL.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEL.appendChild(tagEl)
+    })
+}
+
+const randomSelect = () => {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        if(randomTag != undefined)
+        {
+            highlightTag(randomTag)
+
+            setTimeout(() => {
+                unHighlightTag(randomTag)
+            }, 100)
+        }
+    }, 100 );
+    
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        },100)
+    }, times*120)
+}
+
+
+const pickRandomTag = () => {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+const highlightTag = (tag) => {
+  tag.classList.add('highlight');
+}
+
+const unHighlightTag = (tag) => {
+    tag.classList.remove('highlight');
+}
